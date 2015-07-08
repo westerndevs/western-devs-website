2f984ad21cace19e9e07e98925a31a30a95f52cc
Kyle Baley <kyle@baley.org>
2015-07-07 21:48:56 -0400

    Update podcasts page to use Azure content

---
+++ _plugins/render_podcast.rb
@@ -0,0 +1,20 @@
+module Jekyll
+  class RenderPodcastTag < Liquid::Tag
+
+    def initialize(tag_name, podcast, tokens )
+      super
+      @podcast = podcast
+    end
+
+    def render(context)
+      podcast = Liquid::Template.parse(@podcast).render context
+      "
+ <audio controls>
+	<source src='https://westerndevs.blob.core.windows.net/podcasts/#{podcast}' type='audio/mpeg'>
+    Your browser does not support the audio element.
+</audio>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('render_podcast', Jekyll::RenderPodcastTag)
