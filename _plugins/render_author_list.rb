a1f0abdda83522c9ab599dcdc4794e8baffdeb96
Kyle Baley <kyle@baley.org>
2015-07-16 08:41:15 -0400

    Address PR comments

--- _plugins/render_author_list.rb
+++ _plugins/render_author_list.rb
@@ -4,11 +4,17 @@ require 'uri'
 module Jekyll
   module AuthorFilter
     def to_author_list(input)
-      url = @context.registers[:site].config['url']
-      author_list = @context.registers[:site].data["authors"]
-      input.split(',').map{|x| "<a href='#{url}/bios/#{x}''>#{author_list[x]['name']}</a>"}.join(', ')
+      input.split(',').map do |x|
+        get_author_link x 
+      end.join(', ')
     end
 
+    private 
+    def get_author_link(author_token)
+      url = @context.registers[:site].config['url']
+      author = @context.registers[:site].data["authors"][author_token]
+      "<a href='#{url}/bios/#{author_token}'>#{author['name']}</a>"
+    end
   end
 end
 
