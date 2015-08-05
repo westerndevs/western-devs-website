120c18c90be9882b5c773becac15c63dfb853af8
DylanSmith <optikal@shaw.ca>
2015-08-05 02:56:40 -0500

    replaced putty/plink with dockerextension/compose

--- _azure/deploy.ps1
+++ _azure/deploy.ps1
@@ -4,14 +4,6 @@ Import-Module Azure -ErrorAction SilentlyContinue
 $EnvName = Read-Host "Name for Azure Resource Group (must be globally unique and all lowercase)?"
 $BranchName = Read-Host "Name of branch in git?"
 
-$RemoteScriptPath = Join-Path $PSScriptRoot "putty.sh"
-$UpdatedRemoteScriptPath = Join-Path $PSScriptRoot "$EnvName.sh"
-
-$BashFile = (Get-Content $RemoteScriptPath) | Foreach-Object {$_ -replace '\[resourceGroup\]',$EnvName}
-$BashFile = $BashFile | Foreach-Object {$_ -replace '\[branchName\]',$BranchName}
-$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
-[System.IO.File]::WriteAllLines($UpdatedRemoteScriptPath, $BashFile, $Utf8NoBomEncoding)
-
 Add-AzureAccount
 Get-AzureSubscription | Format-Wide
 $AzureSub = Read-Host "What Azure Subscription (one of the values above)?"
@@ -20,20 +12,8 @@ Select-AzureSubscription $AzureSub
 Switch-AzureMode AzureResourceManager
 
 $TemplateFile = Join-Path $PSScriptRoot "wddocker.json"
-New-AzureResourceGroup -Location "West US" -Name $EnvName -TemplateFile $TemplateFile -verbose
+$params = @{branchName="$BranchName"}
+New-AzureResourceGroup -Location "West US" -Name $EnvName -TemplateFile $TemplateFile -TemplateParameterObject $params -verbose
 
-$PlinkPath = Join-Path $PSScriptRoot "plink.exe"
-$PlinkArgs = "-ssh $EnvName.westus.cloudapp.azure.com -l WesternDevs -pw P2ssw0rd -m $UpdatedRemoteScriptPath"
-
-# This will suppress the prompt that the server is untrusted for all future plink commands
-Write-Host "Note: The below error is expected - I just haven't figured out how to suppress the scary error text yet"
-Invoke-Expression "echo y | $PlinkPath -ssh WesternDevs@$EnvName.westus.cloudapp.azure.com 'exit'" -ErrorAction SilentlyContinue | Out-Null
-Write-Host ""
-Write-Host "======================"
-
-Write-Host "Running: $PlinkPath $PlinkArgs"
-Start-Process $PlinkPath -ArgumentList $PlinkArgs
-Write-Host "======================"
-
-Write-Host "Once the plink command window says Server Running you can browse to your site at http://$EnvName.westus.cloudapp.azure.com:4000"
-Read-Host "Press enter to close this window..."
+Read-Host "Press enter to launch a browser to your new site - if it gives an error wait a minute then refresh"
+Start-Process "http://$EnvName.westus.cloudapp.azure.com:4000"
--- _azure/putty.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/westerndevs/western-devs-website.git
-cd western-devs-website
-git checkout [branchName]
-sed -i 's/www.westerndevs.com/[resourceGroup].westus.cloudapp.azure.com:4000/g' _config.yml
-docker run -t -p 4000:4000 -v ~/western-devs-website:/root/jekyll abarylko/western-devs:v1 sh -c 'bundle install && jekyll serve --host 0.0.0.0 --force_polling'
