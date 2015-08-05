fdc8014c44c5d5c32825e6fa9375fbb492cbf501
DylanSmith <optikal@shaw.ca>
2015-08-04 20:48:53 -0500

    made some updates to support a user branch in the azure stuff

--- _azure/deploy.ps1
+++ _azure/deploy.ps1
@@ -1,10 +1,11 @@
 ﻿$EnvName = Read-Host "Name for Azure Resource Group (must be globally unique and all lowercase)?"
+$BranchName = Read-Host "Name of branch in git?"
 
 $RemoteScriptPath = Join-Path $PSScriptRoot "putty.sh"
 $UpdatedRemoteScriptPath = Join-Path $PSScriptRoot "$EnvName.sh"
-$BashFile = (Get-Content $RemoteScriptPath) | Foreach-Object {$_ -replace '\[resourceGroup\]',$EnvName}
-$TemplateFile = Join-Path $PSScriptRoot "wddocker.json"
 
+$BashFile = (Get-Content $RemoteScriptPath) | Foreach-Object {$_ -replace '\[resourceGroup\]',$EnvName}
+$BashFile = $BashFile | Foreach-Object {$_ -replace '\[branchName\]',$BranchName}
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
 [System.IO.File]::WriteAllLines($UpdatedRemoteScriptPath, $BashFile, $Utf8NoBomEncoding)
 
@@ -15,13 +16,17 @@ Select-AzureSubscription $AzureSub
 
 Switch-AzureMode AzureResourceManager
 
+$TemplateFile = Join-Path $PSScriptRoot "wddocker.json"
 New-AzureResourceGroup -Location "West US" -Name $EnvName -TemplateFile $TemplateFile -verbose
 
 $PlinkPath = Join-Path $PSScriptRoot "plink.exe"
 $PlinkArgs = "-ssh $EnvName.westus.cloudapp.azure.com -l WesternDevs -pw P2ssw0rd -m $UpdatedRemoteScriptPath"
 
+# This will suppress the prompt that the server is untrusted for all future plink commands
+Write-Host "Note: The below error is expected - I just haven't figured out how to suppress the scary red text yet"
+Invoke-Expression "echo y | $PlinkPath -ssh WesternDevs@$EnvName.westus.cloudapp.azure.com 'exit'" -ErrorAction SilentlyContinue | Out-Null
+
 Write-Host "Running: $PlinkPath $PlinkArgs"
-Invoke-Expression "echo y | $PlinkPath -ssh WesternDevs@$EnvName.westus.cloudapp.azure.com 'exit'" | Out-Null
 Start-Process $PlinkPath -ArgumentList $PlinkArgs
 
 Write-Host "Once the plink command window says Server Running you can browse to your site at http://$EnvName.westus.cloudapp.azure.com:4000"
--- _azure/putty.sh
+++ _azure/putty.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/westerndevs/western-devs-website.git
-sed -i 's/www.westerndevs.com/[resourceGroup].westus.cloudapp.azure.com:4000/g' western-devs-website/_config.yml
+cd western-devs-website
+git checkout [branchName]
+sed -i 's/www.westerndevs.com/[resourceGroup].westus.cloudapp.azure.com:4000/g' _config.yml
 docker run -t -p 4000:4000 -v ~/western-devs-website:/root/jekyll abarylko/western-devs:v1 sh -c 'bundle install && jekyll serve --host 0.0.0.0 --force_polling'
