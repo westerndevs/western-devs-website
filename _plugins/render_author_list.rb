2ebe1aca9421264a1c70a1abee82df6d820e13e8
Kyle Baley <kyle@baley.org>
2015-09-05 18:12:47 -0400

    Add podcasts; start on published works

--- _plugins/render_author_list.rb
+++ _plugins/render_author_list.rb
@@ -21,6 +21,37 @@ module Jekyll
     end
 
   end
+
+  module PostFilter
+    def filter_by_author(posts, author)
+      filtered_posts = []
+      for post in posts
+        found_author = post["participants"].find { |v| v == author }
+        if found_author
+          filtered_posts.push(post)
+        end
+      end
+      filtered_posts
+    end
+  end
+
+  module PublicationFilter
+    def filter_work_by_author(publications, author)
+      # books = @context.registers[:site].data["publications"]["books"].find { |v| v["author"] == author }
+      books = publications["books"].find{ |v| v["author"] == author}
+      works = []
+      if books
+        puts "Book: " + books["title"].to_s
+        for book in books
+          works.push(book)
+        end 
+      end
+      works
+    end
+  end
 end
 
 Liquid::Template.register_filter(Jekyll::AuthorFilter)
+Liquid::Template.register_filter(Jekyll::PostFilter)
+Liquid::Template.register_filter(Jekyll::PublicationFilter)
+
